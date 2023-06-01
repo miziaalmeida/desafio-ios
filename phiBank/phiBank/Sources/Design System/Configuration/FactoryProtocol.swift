ae0e44fb528724268f30851e9d845ceabbf5da5a
mizialima <mizia.alima@gmail.com>
2023-06-01 00:35:34 -0300

    add structure, json and initial configurations in project

---
+++ phiBank/phiBank/Sources/Design System/Configuration/FactoryProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  FactoryProtocol.swift
+//  phiBank
+//
+//  Created by Mizia Lima on 01/06/23.
+//
+
+import UIKit
+//
+//public protocol WorkerProtocol {
+//    func createWorker()
+//}
+//
+//public protocol FactoryProtocol {
+//    func createViewController() -> UIViewController
+//}
+//
+//class PhiDesignSystemFactory: FactoryProtocol {
+//    func createViewController() -> UIViewController {
+//        let worker: WorkerProtocol
+//    }
+//}
---
+++ phiBank/phiBank/Sources/Design System/Tokens/PhiDSTokensColors.swift
@@ -0,0 +1,16 @@
+//
+//  PhiDSTokensColors.swift
+//  phiBank
+//
+//  Created by Mizia Lima on 31/05/23.
+//
+
+import UIKit
+
+/// Colors specialized `PhiDSTokensColors`
+public enum PhiDSTokensColors {
+    static let gray = UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1)
+    static let black = UIColor(red: 32/255, green: 32/255, blue: 33/255, alpha: 1)
+    static let white = UIColor(ciColor: .white)
+    static let green = UIColor(red: 0/255, green: 193/255, blue: 175/255, alpha: 1)
+}
